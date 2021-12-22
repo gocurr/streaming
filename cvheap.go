c92f629eaf70ecacc26a13f9e1e638eed68b0a0e
jes <sample@gmail.com>
2021-12-22 10:34:51 +0800

    heap

---
+++ cvheap.go
@@ -0,0 +1,28 @@
+package streaming
+
+// CountVal wraps Count-Val
+type CountVal struct {
+	Count int
+	Val   interface{}
+}
+
+// An cvHeap is a max-heap of CountValues.
+type cvHeap []CountVal
+
+func (h cvHeap) Len() int           { return len(h) }
+func (h cvHeap) Less(i, j int) bool { return h[i].Count > h[j].Count }
+func (h cvHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *cvHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	*h = append(*h, x.(CountVal))
+}
+
+func (h *cvHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
--- streaming.go
+++ streaming.go
@@ -369,33 +369,6 @@ func (s *Stream) Element(i int) interface{} {
 	return nil
 }
 
-// CountVal wraps Count-Val
-type CountVal struct {
-	Count int
-	Val   interface{}
-}
-
-// An cvHeap is a max-heap of CountValues.
-type cvHeap []CountVal
-
-func (h cvHeap) Len() int           { return len(h) }
-func (h cvHeap) Less(i, j int) bool { return h[i].Count > h[j].Count }
-func (h cvHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-
-func (h *cvHeap) Push(x interface{}) {
-	// Push and Pop use pointer receivers because they modify the slice's length,
-	// not just its contents.
-	*h = append(*h, x.(CountVal))
-}
-
-func (h *cvHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
 // Top returns a stream consisting of n elements that appear most often
 func (s *Stream) Top(n int) *Stream {
 	prev := s.prevPipeline()
