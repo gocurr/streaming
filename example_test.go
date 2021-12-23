4861859f6fdcda40f56073576759381e1fecf2e8
jes <sample@gmail.com>
2021-12-23 22:33:21 +0800

    enlarge testdata in example_test.go

--- example_test.go
+++ example_test.go
@@ -4,15 +4,18 @@ import (
 	"fmt"
 	"github.com/gocurr/streaming"
 	"io/ioutil"
+	"log"
 	"strings"
 )
 
-func Example_stream() {
+func read() []byte {
 	file, err := ioutil.ReadFile("testdata/Isaac.Newton-Opticks.txt")
 	if err != nil {
-		return
+		log.Fatalln(err)
 	}
-
+	return file
+}
+func handle(file []byte) {
 	streaming.
 		Of(streaming.Strings{string(file)}).
 		FlatMap(func(i interface{}) streaming.Slicer {
@@ -31,6 +34,24 @@ func Example_stream() {
 			fmt.Printf("%s ", v.Val)
 		})
 	fmt.Println("?")
+}
 
+func Example_stream() {
+	handle(read())
 	// Output: to be or not ?
+}
+
+func Example_largeFile() {
+	file := read()
+	for i := 0; i < 10; i++ {
+		// enlarge the testdata
+		// size = original-size << 10
+		// size = 567198*1024 = 580810752 ≈ 550MB
+		file = append(file, file...)
+	}
+	fmt.Println(len(file))
+	// lower than 2GB memory cost in general
+	handle(file)
+	// Output: 580810752
+	// to be or not ?
 }
